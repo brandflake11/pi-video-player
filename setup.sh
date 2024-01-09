df69d627bafd4f23735dc5fce67f165cff8d8d9d
Brandon Hale <bthaleproductions@gmail.com>
2024-01-09 15:35:45 -0500

    Made the setup script more verbose and talk to the user more.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# To begin with, you will need to set the wifi region/country
+echo "To begin with, you will need to set the wifi region/country"
+read -rp "Press enter to continue"
 
 # Update the pi and install goodies
+echo "Updating and installing essential programs"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y mpv emacs
 
 # Set the audio output to HDMI
 # Use pactl list short sinks to list options, but hopefully if using pi 4s the hardware is the same
+echo "Setting HDMI audio out as the default sound output"
 pactl set-default-sink 76
 
 # Create the autostart file for the user (not the system)
 
 if [ ! -d "$HOME/.config/lxsession" ]
    then
+       echo "Creating autostart file"
        mkdir "$HOME/.config/lxsession"
        mkdir "$HOME/.config/lxsession/LXDE-pi"
        cp -v /etc/xdg/lxsession/LXDE-pi/autostart "$HOME/.config/lxsession/LXDE-pi/"
@@ -21,11 +25,12 @@ if [ ! -d "$HOME/.config/lxsession" ]
        STARTUP="$HOME/.config/lxsession/LXDE-pi/autostart"
        echo "@vlc -L --no-osd -f $HOME/Videos" >> "$STARTUP"
 else
-    echo "$HOME/.config/lxsession/LXDE-pi/autostart exists, not copying the startup."
+    echo "$HOME/.config/lxsession/LXDE-pi/autostart exists, not creating the startup."
 fi
 
 # Install the teamviewer binary
 # Just going to download it, as it doesn't really work with wayland now
+echo "Downloading Teamviewer deb in case you need it."
 wget -p "$HOME/Downloads" https://download.teamviewer.com/download/linux/teamviewer-host_arm64.deb
 # yes | sudo dpkg -i ~/Downloads/teamviewer-host_arm64.deb
 # Installs dependencies for teamviewer
@@ -33,5 +38,7 @@ wget -p "$HOME/Downloads" https://download.teamviewer.com/download/linux/teamvie
 # teamviewer &
 
 echo "Lastly, you need to enable VNC support"
-echo "Please go to |Interfaces| and toggle VNC"
+echo "A gui will appear. In it"
+echo "please go to |Interfaces| and toggle VNC"
+read -rp "Press enter to continue"
 rc-gui
